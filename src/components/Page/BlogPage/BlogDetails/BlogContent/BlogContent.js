1a746fd8d77f5bbd18d8a98008629076f41af638
Nischal Batas <nischal.batas88@gmail.com>
2024-09-09 15:00:39 +0545

    removing slider

--- src/components/Page/BlogPage/BlogDetails/BlogContent/BlogContent.js
+++ src/components/Page/BlogPage/BlogDetails/BlogContent/BlogContent.js
@@ -3,19 +3,19 @@ import './style.css'
 const BlogContent = ({data}) => {
   return (
     <div className='main_container blog_container_content inside_sidespace'>
-        <p>{data.attributes.description}</p>
+        <p>{data && data?.attributes?.description}</p>
     <div className='mt-5'>
-    {data.attributes.content.map((item3, index3) => {
+    {data && data?.attributes?.content.map((item3, index3) => {
                   return <p key={index3}>{item3.children[0].text}</p>;
                 })}
     </div>
 
    
-<div className='my-10 text-[#EF6700] border border-[#ff70031f] bg-[#150900] text-[16px] font-semibold tracking-tight px-6 py-4 rounded-[8px]'>{data.attributes.slogan}
+<div className='my-10 text-[#EF6700] border border-[#ff70031f] bg-[#150900] text-[16px] font-semibold tracking-tight px-6 py-4 rounded-[8px]'>{data && data?.attributes?.slogan}
 
 </div>
    <div className='mt-5'>
-   {data.attributes.end_description.map((item4, index4) => {
+   {data && data?.attributes?.end_description.map((item4, index4) => {
                   return <p key={index4}>{item4.children[0].text}</p>;
                 })}
    </div>
