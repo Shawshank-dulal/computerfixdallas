042bf76836aee80fa371f27d612629d4f31b4728
Nischal Batas <nischal.batas88@gmail.com>
2024-09-21 22:09:33 +0545

    adding metatag

--- src/app/(pages)/(protected)/blogs/[slug]/page.js
+++ src/app/(pages)/(protected)/blogs/[slug]/page.js
@@ -9,7 +9,7 @@ const page = async(props) => {
   console.log("POST details blog:",post)
   return (
     <div>
-        {/* <BlogDetails blogslug={blogslug}/> */}
+        <BlogDetails blogslug={blogslug}/>
     </div>
   )
 }
