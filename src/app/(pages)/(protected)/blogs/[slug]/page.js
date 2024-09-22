100f7a1552552dbca94441b40ca05676ce24fe39
Nischal Batas <nischal.batas88@gmail.com>
2024-09-22 11:25:50 +0545

    adding metaTags

--- src/app/(pages)/(protected)/blogs/[slug]/page.js
+++ src/app/(pages)/(protected)/blogs/[slug]/page.js
@@ -1,31 +1,36 @@
-import BlogDetails from '@/components/Page/BlogPage/BlogDetails/BlogDetails'
-import { fetchblogs, fetchSingleblog } from '@/utils/blogs'
-import React from 'react'
+import BlogDetails from "@/components/Page/BlogPage/BlogDetails/BlogDetails";
+import config from "@/config";
+import { fetchSingleblog } from "@/utils/blogs";
+import React from "react";
 
-const page = async(props) => {
-  const blogslug=props.searchParams.id
+const page = async (props) => {
+  const blogslug = props.searchParams.id;
   // console.log("Blogs id:",blogslug)
   const post = await fetchSingleblog(props.searchParams.id);
-  console.log("POST details blog:",post)
-  return (
-    <div>
-        <BlogDetails blogslug={blogslug}/>
-    </div>
-  )
-}
+  console.log("POST details blog:", post);
+  return <div>
+    {/* <BlogDetails blogslug={blogslug}/> */}
+    </div>;
+};
 
-export default page
+export default page;
 
 
-export async function generateMetadata(props){
-  const postData = await  fetchSingleblog(props.searchParams.id);
-  const post=postData.data
+export async function generateMetadata({ params, searchParams }){
+  const blogId = searchParams.id;
+  const blogslug = params.slug;
+  const postData = await fetchSingleblog(blogId);
+  const post=postData.blogs.data
+
+    // Log values to the server console for debugging
+    console.log("Blog ID:", blogId);
+    console.log("Blog Slug:", post);
+
   if (!post) {
     return {}
   }
-  const canonicalUrl = `${process.env.NEXT_PUBLIC_URL}/blogs/${props.params.slug}?id:${props.searchParams.id}`;
 
-
+  const canonicalUrl = `${process.env.NEXT_PUBLIC_URL}/blogs/${blogslug}?id:${blogId}`;
   return {
     title: post.attributes.title,
     description: post.attributes.description,
@@ -54,7 +59,7 @@ export async function generateMetadata(props){
       post.attributes.description,
       creator: post.attributes.title,
       images: {
-        url: `${config.api}${data?.attributes?.image?.data?.attributes?.url}`, // Must be an absolute URL
+        url: `${config.api}${post?.attributes?.image?.data?.attributes?.url}`, // Must be an absolute URL
         alt: post.attributes.title,
       },
     },
