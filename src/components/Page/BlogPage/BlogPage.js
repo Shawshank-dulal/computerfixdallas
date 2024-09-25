600509e9c5ccd6184da06e3cb40e3112102ba067
Nischal Batas <nischal.batas88@gmail.com>
2024-09-25 22:54:05 +0545

    adding iframe

--- src/components/Page/BlogPage/BlogPage.js
+++ src/components/Page/BlogPage/BlogPage.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import Introduction from './Content/Introduction/Introduction'
-import Blogs from './Content/BlogItems/Blogs'
-import Categories from './Content/Categories/Categories'
-import './Content/Categories/style.css'
+import React from "react";
+import Introduction from "./Content/Introduction/Introduction";
+import Blogs from "./Content/BlogItems/Blogs";
+import Categories from "./Content/Categories/Categories";
+import "./Content/Categories/style.css";
 
 const BlogPage = () => {
   return (
-    <div className=''>
-        <Introduction/>
-  
-     <Categories/>
+    <div className="">
+      <Introduction />
+      <Categories />
     </div>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
