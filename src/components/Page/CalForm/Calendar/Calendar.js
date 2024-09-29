1249dda6a05c88a79a032637a9f900c5494b0996
shawshank-dulal <sashank.dulal@gmail.com>
2024-09-29 19:19:26 +0545

    update cal integration to support@computefixdallas

--- src/components/Page/CalForm/Calendar/Calendar.js
+++ src/components/Page/CalForm/Calendar/Calendar.js
@@ -1,27 +1,30 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import Cal, { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
 
 const Calendar = () => {
-	useEffect(()=>{
-        (async function () {
-          const cal = await getCalApi({});
-          cal("ui", {theme: "dark","styles":{"branding":{"brandColor":"#000000"}},"hideEventTypeDetails":false,"layout":"month_view"});
-        })();
-      }, [])
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({});
+      cal("ui", {
+        theme: "dark",
+        styles: { branding: { brandColor: "#000000" } },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
+  }, []);
 
-   
   return (
-    <div >
-     <Cal 
-    calLink="npanthi/15min"
-    style={{maxWidth:"100%",minHeight:"490px",overflow:"auto"}}
-    config={{layout: 'month_view'}}
-  
-    
-  /></div>
-  )
-}
+    <div>
+      <Cal
+        calLink={process.env.NEXT_PUBLIC_CAL_KEY}
+        style={{ maxWidth: "100%", minHeight: "490px", overflow: "auto" }}
+        config={{ layout: "month_view" }}
+      />
+    </div>
+  );
+};
 
-export default Calendar
+export default Calendar;
